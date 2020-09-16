31757f1f7c551ee850b9670f90652963ac850c37
Jahan Gagan <jahanggn9@gmail.com>
2020-09-16 17:07:22 +0530

    FIX: trust level to its titles

--- javascripts/discourse/initializers/trust-level-title-on-post.js
+++ javascripts/discourse/initializers/trust-level-title-on-post.js
@@ -1,24 +1,29 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 import { h } from "virtual-dom";
-import I18n from "I18n";
 
 export default {
-  name: "tl-init",
+  name: "trust-level-title-on-post",
   initialize(container) {
-    withPluginApi("0.8.7", postTlInit);
+    withPluginApi("0.8.7", trustLevelTitleOnPost);
   },
 };
 
-const postTlInit = (api) => {
+const trustLevelTitleOnPost = (api) => {
   api.reopenWidget("poster-name", {
     html(attrs) {
+      let titles = new Map([
+        [0, "new user"],
+        [1, "basic user"],
+        [2, "member"],
+        [3, "regular"],
+        [4, "leader"],
+      ]);
       let model = api._lookupContainer("controller:topic").model;
       let myPost = model.get("postStream.posts").find((x) => x.id === attrs.id);
       let trust_level = myPost.trust_level;
+      let trust_level_title = titles.get(trust_level);
       const contents = this._super(...arguments);
-      contents.push(
-        h("span.trust-level", I18n.t(themePrefix("tl"), { level: trust_level }))
-      );
+      contents.push(h("span.trust-level", trust_level_title));
       return contents;
     },
   });
